bb3a3c6cb99c96f3b1eeb00cd707400518005971
Haukur Bodvarsson <haukurmar@gmail.com>
2021-11-26 16:15:17 +0100

    feat(main-ui): added css reset

---
+++ packages/main-ui/src/cssReset.ts
@@ -0,0 +1,930 @@
+// https://meyerweb.com/eric/tools/css/reset/
+
+export const meyerResetObj = {
+  html: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  body: { lineHeight: '1' },
+  div: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  span: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  applet: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  object: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  iframe: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  h1: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  h2: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  h3: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  h4: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  h5: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  h6: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  p: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  blockquote: { quotes: 'none' },
+  pre: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  a: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  abbr: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  acronym: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  address: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  big: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  cite: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  code: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  del: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  dfn: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  em: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  img: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  ins: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  kbd: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  q: { quotes: 'none' },
+  s: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  samp: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  small: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  strike: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  strong: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  sub: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  sup: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  tt: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  var: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  b: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  u: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  i: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  center: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  dl: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  dt: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  dd: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  ol: { listStyle: 'none' },
+  ul: { listStyle: 'none' },
+  li: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  fieldset: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  form: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  label: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  legend: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  table: { borderCollapse: 'collapse', borderSpacing: '0' },
+  caption: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  tbody: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  tfoot: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  thead: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  tr: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  th: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  td: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  article: { display: 'block' },
+  aside: { display: 'block' },
+  canvas: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  details: { display: 'block' },
+  embed: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  figure: { display: 'block' },
+  figcaption: { display: 'block' },
+  footer: { display: 'block' },
+  header: { display: 'block' },
+  hgroup: { display: 'block' },
+  menu: { display: 'block' },
+  nav: { display: 'block' },
+  output: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  ruby: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  section: { display: 'block' },
+  summary: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  time: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  mark: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  audio: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  video: {
+    margin: '0',
+    padding: '0',
+    border: '0',
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+  },
+  blockquote_before: { content: 'none' },
+  blockquote_after: { content: 'none' },
+  q_before: { content: 'none' },
+  q_after: { content: 'none' },
+};
+
+export const meyerCssReset = `html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}`;
+
+export const normalizeCss = `/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+
+/* Document
+   ========================================================================== */
+
+/**
+ * 1. Correct the line height in all browsers.
+ * 2. Prevent adjustments of font size after orientation changes in iOS.
+ */
+
+html {
+  line-height: 1.15; /* 1 */
+  -webkit-text-size-adjust: 100%; /* 2 */
+}
+
+/* Sections
+   ========================================================================== */
+
+/**
+ * Remove the margin in all browsers.
+ */
+
+body {
+  margin: 0;
+}
+
+/**
+ * Render the \`main\` element consistently in IE.
+ */
+
+main {
+  display: block;
+}
+
+/**
+ * Correct the font size and margin on \`h1\` elements within \`section\` and
+ * \`article\` contexts in Chrome, Firefox, and Safari.
+ */
+
+h1 {
+  font-size: 2em;
+  margin: 0.67em 0;
+}
+
+/* Grouping content
+   ========================================================================== */
+
+/**
+ * 1. Add the correct box sizing in Firefox.
+ * 2. Show the overflow in Edge and IE.
+ */
+
+hr {
+  box-sizing: content-box; /* 1 */
+  height: 0; /* 1 */
+  overflow: visible; /* 2 */
+}
+
+/**
+ * 1. Correct the inheritance and scaling of font size in all browsers.
+ * 2. Correct the odd \`em\` font sizing in all browsers.
+ */
+
+pre {
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
+}
+
+/* Text-level semantics
+   ========================================================================== */
+
+/**
+ * Remove the gray background on active links in IE 10.
+ */
+
+a {
+  background-color: transparent;
+}
+
+/**
+ * 1. Remove the bottom border in Chrome 57-
+ * 2. Add the correct text decoration in Chrome, Edge, IE, Opera, and Safari.
+ */
+
+abbr[title] {
+  border-bottom: none; /* 1 */
+  text-decoration: underline; /* 2 */
+  text-decoration: underline dotted; /* 2 */
+}
+
+/**
+ * Add the correct font weight in Chrome, Edge, and Safari.
+ */
+
+b,
+strong {
+  font-weight: bolder;
+}
+
+/**
+ * 1. Correct the inheritance and scaling of font size in all browsers.
+ * 2. Correct the odd \`em\` font sizing in all browsers.
+ */
+
+code,
+kbd,
+samp {
+  font-family: monospace, monospace; /* 1 */
+  font-size: 1em; /* 2 */
+}
+
+/**
+ * Add the correct font size in all browsers.
+ */
+
+small {
+  font-size: 80%;
+}
+
+/**
+ * Prevent \`sub\` and \`sup\` elements from affecting the line height in
+ * all browsers.
+ */
+
+sub,
+sup {
+  font-size: 75%;
+  line-height: 0;
+  position: relative;
+  vertical-align: baseline;
+}
+
+sub {
+  bottom: -0.25em;
+}
+
+sup {
+  top: -0.5em;
+}
+
+/* Embedded content
+   ========================================================================== */
+
+/**
+ * Remove the border on images inside links in IE 10.
+ */
+
+img {
+  border-style: none;
+}
+
+/* Forms
+   ========================================================================== */
+
+/**
+ * 1. Change the font styles in all browsers.
+ * 2. Remove the margin in Firefox and Safari.
+ */
+
+button,
+input,
+optgroup,
+select,
+textarea {
+  font-family: inherit; /* 1 */
+  font-size: 100%; /* 1 */
+  line-height: 1.15; /* 1 */
+  margin: 0; /* 2 */
+}
+
+/**
+ * Show the overflow in IE.
+ * 1. Show the overflow in Edge.
+ */
+
+button,
+input { /* 1 */
+  overflow: visible;
+}
+
+/**
+ * Remove the inheritance of text transform in Edge, Firefox, and IE.
+ * 1. Remove the inheritance of text transform in Firefox.
+ */
+
+button,
+select { /* 1 */
+  text-transform: none;
+}
+
+/**
+ * Correct the inability to style clickable types in iOS and Safari.
+ */
+
+button,
+[type="button"],
+[type="reset"],
+[type="submit"] {
+  -webkit-appearance: button;
+}
+
+/**
+ * Remove the inner border and padding in Firefox.
+ */
+
+button::-moz-focus-inner,
+[type="button"]::-moz-focus-inner,
+[type="reset"]::-moz-focus-inner,
+[type="submit"]::-moz-focus-inner {
+  border-style: none;
+  padding: 0;
+}
+
+/**
+ * Restore the focus styles unset by the previous rule.
+ */
+
+button:-moz-focusring,
+[type="button"]:-moz-focusring,
+[type="reset"]:-moz-focusring,
+[type="submit"]:-moz-focusring {
+  outline: 1px dotted ButtonText;
+}
+
+/**
+ * Correct the padding in Firefox.
+ */
+
+fieldset {
+  padding: 0.35em 0.75em 0.625em;
+}
+
+/**
+ * 1. Correct the text wrapping in Edge and IE.
+ * 2. Correct the color inheritance from \`fieldset\` elements in IE.
+ * 3. Remove the padding so developers are not caught out when they zero out
+ *    \`fieldset\` elements in all browsers.
+ */
+
+legend {
+  box-sizing: border-box; /* 1 */
+  color: inherit; /* 2 */
+  display: table; /* 1 */
+  max-width: 100%; /* 1 */
+  padding: 0; /* 3 */
+  white-space: normal; /* 1 */
+}
+
+/**
+ * Add the correct vertical alignment in Chrome, Firefox, and Opera.
+ */
+
+progress {
+  vertical-align: baseline;
+}
+
+/**
+ * Remove the default vertical scrollbar in IE 10+.
+ */
+
+textarea {
+  overflow: auto;
+}
+
+/**
+ * 1. Add the correct box sizing in IE 10.
+ * 2. Remove the padding in IE 10.
+ */
+
+[type="checkbox"],
+[type="radio"] {
+  box-sizing: border-box; /* 1 */
+  padding: 0; /* 2 */
+}
+
+/**
+ * Correct the cursor style of increment and decrement buttons in Chrome.
+ */
+
+[type="number"]::-webkit-inner-spin-button,
+[type="number"]::-webkit-outer-spin-button {
+  height: auto;
+}
+
+/**
+ * 1. Correct the odd appearance in Chrome and Safari.
+ * 2. Correct the outline style in Safari.
+ */
+
+[type="search"] {
+  -webkit-appearance: textfield; /* 1 */
+  outline-offset: -2px; /* 2 */
+}
+
+/**
+ * Remove the inner padding in Chrome and Safari on macOS.
+ */
+
+[type="search"]::-webkit-search-decoration {
+  -webkit-appearance: none;
+}
+
+/**
+ * 1. Correct the inability to style clickable types in iOS and Safari.
+ * 2. Change font properties to \`inherit\` in Safari.
+ */
+
+::-webkit-file-upload-button {
+  -webkit-appearance: button; /* 1 */
+  font: inherit; /* 2 */
+}
+
+/* Interactive
+   ========================================================================== */
+
+/*
+ * Add the correct display in Edge, IE 10+, and Firefox.
+ */
+
+details {
+  display: block;
+}
+
+/*
+ * Add the correct display in all browsers.
+ */
+
+summary {
+  display: list-item;
+}
+
+/* Misc
+   ========================================================================== */
+
+/**
+ * Add the correct display in IE 10+.
+ */
+
+template {
+  display: none;
+}
+
+/**
+ * Add the correct display in IE 10.
+ */
+
+[hidden] {
+  display: none;
+}`;
--- packages/main-ui/src/index.ts
+++ packages/main-ui/src/index.ts
@@ -1,2 +1,3 @@
 export * from './MainNav';
 export * from './LayoutWrapper';
+export * from './cssReset';
