1892b26a87aaaa80d467d4b9cb09eb3094365890
Haukur Bodvarsson <haukurmar@gmail.com>
2021-11-26 16:34:18 +0100

    feat(main-ui): added a Heading component

---
+++ packages/main-ui/src/Heading.tsx
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import { useHeadingStyles } from './headingStyles';
+import { ReactNode } from 'react';
+
+type HeadingType = 'main' | 'sub';
+
+type HeadingProps = {
+  children?: ReactNode;
+} & HeadingDefaultProps;
+
+type HeadingDefaultProps = {
+  type?: 'main' | 'sub';
+  size?: 'small' | 'regular' | 'large';
+};
+
+const defaultProps: HeadingDefaultProps = {
+  type: 'main',
+  size: 'regular',
+};
+
+const Heading = (props: HeadingProps) => {
+  const styles = useHeadingStyles(props);
+
+  return <h1 css={styles.root}>{props.children}</h1>;
+};
+
+Heading.defaultProps = defaultProps;
+
+export { Heading };
+export type { HeadingProps };
--- packages/main-ui/src/index.ts
+++ packages/main-ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './MainNav';
+export * from './Heading';
 export * from './LayoutWrapper';
 export * from './cssReset';
